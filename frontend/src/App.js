692e76625e0f9c9421e1f589af51890144aa0dc6
SvnvmD <someodargay167@gmail.com>
2025-05-04 09:45:21 +1000

    merge with main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,8 @@ import "./App.css";
 import Home from "./pages/Home";
 //import Navbar from "./components/Navbar";
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login1 from "./pages/Login1";
+import Register1 from "./pages/Register1";
 import UserPage from "./pages/UserPage";
 
 // Wrapper to conditionally show Navbar
@@ -29,8 +29,8 @@ function AppWrapper() {
       {/* {!isAdminRoute && <Navbar />} */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login1" element={<Login1 />} />
+        <Route path="/register1" element={<Register1 />} />
         <Route path="/userpage" element={<UserPage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/events" element={<Events />} />
