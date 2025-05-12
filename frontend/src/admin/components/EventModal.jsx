06da78e2bf1ae9d525f53c2fb207da53e309dc3a
Prashant Rizal <prsnt.rizal@gmail.com>
2025-05-12 12:47:21 +1000

    Minor bug fix

--- frontend/src/admin/components/EventModal.jsx
+++ frontend/src/admin/components/EventModal.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import axiosInstance from "../../axiosConfig";
-import { eventBus } from "./EventBus";
 
 const EventModal = ({
   show,
