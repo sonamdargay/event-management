9303c37d3a83d655bb075190ad1a53e3e2e403b9
Prashant Rizal <prsnt.rizal@gmail.com>
2025-05-21 08:55:57 +1000

    Fixed Paypal Link

--- frontend/src/components/PaypalAdapter.jsx
+++ frontend/src/components/PaypalAdapter.jsx
@@ -3,6 +3,6 @@ import { FundingAdapter } from "./FundingAdapter";
 export class PaypalAdapter extends FundingAdapter {
 
     makePayment() {
-        window.open("https://www.paypal.com/donate/?token=9BiOl3dKVTK8lgeSgxk7l-TK1LN37Y6hVeiL0fDxhTZWlX7LlHoKq3VfwvbyohHvgb66qxQ_6QZiSPfuSxaokfS87H4&sdkMeta=eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWxvYmplY3RzLmNvbS9kb25hdGUvc2RrL2RvbmF0ZS1zZGsuanMiLCJhdHRycyI6eyJkYXRhLXVpZCI6ImVkMjcyYzdkZjNfbWRpNm16aTZtemsifX0&targetMeta=eyJ6b2lkVmVyc2lvbiI6IjlfMF81OCIsInRhcmdldCI6IkRPTkFURSIsInNka1ZlcnNpb24iOiIwLjguMCJ9", '_blank')
+        window.open("https://www.paypal.com/pools/c/9eLqsv2Q4h", '_blank')
     }
 }
